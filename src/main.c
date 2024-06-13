85593e4788dee62dbe39173260d23062bf0ffc72
David Konsumer <konsumer@jetboystudio.com>
2024-06-13 11:28:05 -0700

    start working

--- src/main.c
+++ src/main.c
@@ -52,7 +52,7 @@ int main(void)
 
             ClearBackground(RAYWHITE);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            DrawText("Congrats! You created your first window!", 180, 200, 20, LIGHTGRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
